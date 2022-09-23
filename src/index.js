5e3b5be4d22030345f6361898a75ad96803d586b
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-09-23 11:42:27 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form'
-
+import Form2 from './components/ ParentComponent';
 ReactDOM.render(
-  <Form />,
+  <>
+   
+   <Form2></Form2>
+  </>,
   document.getElementById('root')
 );
